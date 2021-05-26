bdcee1e1ab1045fe2e101dcad963462734c80120
Guilherme Moraes <gui.moraes@live.com>
2021-05-26 14:41:10 -0300

    Testando schedule do bot

--- bot.py
+++ bot.py
@@ -1,28 +1,34 @@
 import tweepy
 import schedule
 import time
+import os
 from mensagens import Mensagens
+from dotenv import load_dotenv, find_dotenv
 
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+KEY = os.environ.get("KEY")
+S_KEY = os.environ.get("S_KEY")
+TOKEN = os.environ.get("TOKEN")
+TOKEN_S = os.environ.get("TOKEN_S")
 
 class Bot(Mensagens):   
     def __init__(self, API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
         auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth)
-
-    def publicar(self, mensagem):
+    
+    def publicar(self, mensagem, nome_do_arquivo):
         try:
-            self.api.update_with_media(filename=mensagens.caminho_meme, )
+            self.api.update_with_media(filename=nome_do_arquivo, status=mensagem )
         except:
             self.api.update_status(mensagem)
 
-    def atualizar(self):
-        mensagens = Mensagens()
-        return mensagens
 
     def agendar(self):
-        schedule.every().day.do(self.atualizar)
-        schedule.every().day.at("18:00").do(self.publicar, mensagens.mensagem))
+        schedule.every().day.at("18:00").do(job_func=self.publicar, mensagem=Mensagens().tweet, nome_do_arquivo=Mensagens().caminho_da_imagem_de_hoje)
         while True:
             schedule.run_pending()
             time.sleep(1)
--- mensagens.py
+++ mensagens.py
@@ -11,15 +11,11 @@ class Mensagens:
         
         self.hoje()
         self.caminho_meme(_dia_do_tweet)
-        self.mensagem(_nome_do_arquivo)
+        self.atualizar_tweets()
 
     @property
     def tweet(self):
         return Mensagens._tweet[int(self._dia_do_tweet)]
-    
-    @tweet.setter
-    def tweet(self, novo_tweet):
-        Mensagens._tweet = dict(novo_tweet)
     
     @property
     def caminho_da_imagem_de_hoje(self):
@@ -39,16 +35,19 @@ class Mensagens:
         self._dia_do_tweet = dia_de_hoje
     
     def caminho_meme(self, dia):
-        caminho_imagens = os.path.join(os.getcwd() + '/imagens')
+        caminho_imagens = os.path.join(os.getcwd() + '/imagens/')
         lista_imagens = os.listdir(caminho_imagens)
         nome_do_arquivo_da_imagem_de_hoje = lista_imagens[int(dia)]
         self._nome_do_arquivo = nome_do_arquivo_da_imagem_de_hoje
         self._caminho_da_imagem_de_hoje = caminho_imagens + nome_do_arquivo_da_imagem_de_hoje
-  
-    def mensagem(self, caminho):
-        return self.tweet({int(self._dia_do_tweet): str(caminho.split(".")[0])})
 
-mensagens = Mensagens()
-
-
-print(mensagens.get_dia_do_tweet)
+    def tratar_nome_arquivo(self):
+        nome = self._nome_do_arquivo.split(".")[0]
+        return nome.replace("  ", "\n")
+    
+    def atualizar_tweets(self):
+        caminho_imagens = os.path.join(os.getcwd() + '/imagens')
+        lista_imagens = os.listdir(caminho_imagens)
+        tweet_tratado = self.tratar_nome_arquivo()
+        for len_imagem in range(len(lista_imagens)):
+            Mensagens._tweet[len_imagem] = tweet_tratado
